edc5c21b10fb9a9b612891dc202dced8e4504ea1
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2023-05-15 12:25:01 +1200

    working on num checker

--- 01_main_page_v2.py
+++ 01_main_page_v2.py
@@ -9,6 +9,13 @@ class MainPage:
         # Arial size 14 bold, with white text
         button_font = ("Arial", "10", "bold")
         button_fg = "#FFFFFF"
+        
+        # Initialise variables (such as the feedback variable)
+        self.var_feedback = StringVar()
+        self.var_feedback.set("")
+
+        self.var_has_error = StringVar()
+        self.var_has_error.set("no")
 
         # Set up GUI Frame
         self.main_frame = Frame(padx=10, pady=10)
@@ -48,6 +55,40 @@ class MainPage:
                                 fg="#9C0000")
         self.output_label.grid(row=3)
 
+    # checks user input and if it's valid, converts temp
+    def check_questions(self):
+
+        has_error = "no"
+        error = "Please enter a whole number between 1 and 50"
+
+        response = self.main_entry.get()
+
+        try:
+            response = float(response)
+
+            if response > 0 and response < 51:
+                has_error = "no"
+
+            else:
+                has_error = "yes"
+
+        except ValueError:
+            has_error = "yes"
+
+        # sets var_has_error so that entry box and
+        # labels can be correctly formatted by formatting function
+        if has_error == "yes":
+            self.var_has_error.set("yes")
+            self.var_feedback.set(error)
+            return "invalid"
+        
+        # if we have no errors
+        else:
+            # set to 'no' in case of previous errors
+            self.var_has_error.set("no")
+
+            # return number to be 
+            return response
 
 # main routine
 if __name__ == "__main__":
